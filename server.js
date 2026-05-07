7a2619fad890d9c0a3eb45cf770bd65ed625f5d5
viniciuscelis <vinicuscelis@gmail.com>
2026-05-07 10:24:09 -0300

    feat: adiciona o código da API

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.get('/api/dados', (req,res) => {
     res.json(historico_sensores);
 });
 
+app.get('/api/dados/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const dado = historico_sensores.find(s => s.id === id);
+    if (!dado){
+        return res.status(400).json({mensagem:"ID não encontrado, verifique e tente novamente!"});
+    }
+    res.json(dado);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando com sucesso na porta ${PORT}`)
@@ -23,6 +32,10 @@ app.listen(PORT, () => {
 
 app.post("/api/dados", (req,res) => {
     const{temperatura, umidade, hora} = req.body;
+
+    if(!temperatura||!umidade||!hora){
+        res.status(400).json({mensagem:"Dados incompletos"})
+    }
 
     let novos_dados = {
         id: historico_sensores.length + 1,
@@ -35,3 +48,29 @@ app.post("/api/dados", (req,res) => {
 
     res.status(201).json({dados:novos_dados, mensagem:"Dados enviados com sucesso!" });
 })
+
+app.delete('/api/dados/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const index = historico_sensores.findIndex (s => s.id === id);
+
+    if (index === -1){
+        return res.status(400).json({mensagem:"Dado não encontrado! Verifique e tente novamente"});
+    };
+
+    historico_sensores.splice(index,1);
+
+    res.json({mensagem:"Dados removidos com sucesso!"});
+})
+
+app.put ('api/dados/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const index = historico_sensores.findIndex(s => s.id === id);
+
+    if (index === -1){
+        return res.status(404).json({mensagem:"Não é possível atualizar um dado inexistente!"});
+    };
+
+    const{temperatura,umidade,hora} = req.body;
+    historico_sensores[index] = {id,temperatura,umidade,hora};
+    res.json({mensagem:"Dados atualizados com sucesso!"});
+});
